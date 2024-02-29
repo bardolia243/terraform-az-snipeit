12999ea8e342ceb0403e82783e9d98e95a172843
FAIZAN BARDOLIA <98798345+bardolia243@users.noreply.github.com>
2024-02-29 11:56:15 +0530

    Update provider.tf

--- azure-vm/provider.tf
+++ azure-vm/provider.tf
@@ -9,8 +9,8 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  subscription_id = "49fa5565-762d0-4070-af99-cd932822edbdc"
-  tenant_id       = "7a4b05fe-9637b-4634-b55c-ff8233ff39e94"
+  subscription_id = "49fa5565-76d0-4070-af99-cd92822edbdc"
+  tenant_id       = "7a4b05fe-967b-4634-b55c-ff833ff39e94"
 
   features {
 
